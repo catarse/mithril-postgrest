154e50d3421ba3e1989c37fa447bce715b282324
Diogo Biazus <diogob@gmail.com>
2015-06-09 18:06:10 -0400

    Implementing authentication function WIP

--- src/mithril.postgrest.js
+++ src/mithril.postgrest.js
@@ -9,7 +9,25 @@
 }(function (m, _, localStorage) {
   var postgrest = {};
   postgrest.init = function(apiPrefix){
+
     postgrest.authenticate = function(options){
+      var token = localStorage.getItem("postgrest.token");
+      if(_.isString(token) && token.length > 0){
+        var then = function(callback){ 
+          callback({token: token}) 
+        };
+
+        postgrest.request = function(options){
+          return m.request(_.extend(options, {url: apiPrefix + options.url, config: function(){} }));
+        };
+
+        return {then: then};
+      }
+
+      //TODO: should also redefine postgrest.request
+      return m.request(options).then(function(data){
+        localStorage.setItem("postgrest.token", data.token);
+      });
     };
 
     postgrest.request = function(options){
