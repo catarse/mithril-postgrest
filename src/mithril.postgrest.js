30adc3fd54e39f5dbcfb291ef60d98bea4feb00a
Vinícius Andrade <vchavesster@gmail.com>
2015-06-09 23:32:25 -0300

    Uses mithril deferred to handle promises inside init function

--- src/mithril.postgrest.js
+++ src/mithril.postgrest.js
@@ -11,23 +11,21 @@
   postgrest.init = function(apiPrefix){
 
     postgrest.authenticate = function(options){
-      var token = localStorage.getItem("postgrest.token");
+      var deferred = m.deferred(),
+          token = localStorage.getItem("postgrest.token");
       if(_.isString(token) && token.length > 0){
-        var then = function(callback){ 
-          callback({token: token}) 
-        };
-
-        postgrest.request = function(options){
-          return m.request(_.extend(options, {url: apiPrefix + options.url, config: function(){} }));
-        };
-
-        return {then: then};
+        deferred.resolve({token: token});
+      }else{
+        m.request(options).then(function(data){
+          localStorage.setItem("postgrest.token", data.token);
+          deferred.resolve({token: data.token});
+        });  
       }
+      postgrest.request = function(options){
+        return m.request(_.extend(options, {url: apiPrefix + options.url, config: function(){} }));
+      };
 
-      //TODO: should also redefine postgrest.request
-      return m.request(options).then(function(data){
-        localStorage.setItem("postgrest.token", data.token);
-      });
+      return deferred.promise;
     };
 
     postgrest.request = function(options){
