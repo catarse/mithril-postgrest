3facb7c15f183b9dc7ae0edd944dba40fcf4698e
Diogo Biazus <diogob@gmail.com>
2015-06-11 19:26:38 -0400

    Adds dist files

---
+++ dist/mithril.postgrest.js
@@ -0,0 +1,40 @@
+/*
+    A Mithril.js plugin to authenticate requests against PostgREST
+    Copyright (c) 2007 - 2015 Diogo Biazus
+    Licensed under the MIT license (http://digitalbush.com/projects/masked-input-plugin/#license)
+    Version: 1.0.0
+*/
+!function(factory) {
+    "object" == typeof exports ? factory(require("mithril"), require("underscore"), require("node-localstorage")) : factory(m, _, localStorage);
+}(function(m, _, localStorage) {
+    var postgrest = {}, xhrConfig = function(xhr) {
+        return xhr.setRequestHeader("Authorization", "Bearer " + token()), xhr;
+    }, token = function() {
+        return localStorage.getItem("postgrest.token");
+    };
+    postgrest.reset = function() {
+        localStorage.removeItem("postgrest.token");
+    }, postgrest.init = function(apiPrefix, authenticationOptions) {
+        return postgrest.request = function(options) {
+            return m.request(_.extend(options, {
+                url: apiPrefix + options.url
+            }));
+        }, postgrest.requestWithToken = function(options) {
+            return m.postgrest.authenticate().then(function(data) {
+                var config = _.isFunction(options.config) ? _.compose(options.config, xhrConfig) : xhrConfig;
+                return m.postgrest.request(_.extend(options, {
+                    config: config
+                }));
+            });
+        }, postgrest.authenticate = function() {
+            var deferred = m.deferred();
+            return token() ? deferred.resolve({
+                token: token()
+            }) : m.request(authenticationOptions).then(function(data) {
+                localStorage.setItem("postgrest.token", data.token), deferred.resolve({
+                    token: data.token
+                });
+            }), deferred.promise;
+        }, postgrest;
+    }, m.postgrest = postgrest;
+});
