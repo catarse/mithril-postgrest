585679829232db4ad070c3d311b2c4df1b1831b7
Diogo Biazus <diogob@gmail.com>
2015-09-27 01:06:38 -0400

    Adds support for ES6 compilation

--- karma.conf.js
+++ karma.conf.js
@@ -34,6 +34,19 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'spec/**/*.spec.js': ['babel'],
+      'src/**/*.js': ['babel']
+    },
+    babelPreprocessor: {
+      options: {
+        sourceMap: 'inline'
+      },
+      filename: function(file) {
+        return file.originalPath.replace(/\.js$/, '.es5.js');
+      },
+      sourceFileName: function(file) {
+        return file.originalPath;
+      }
     },
 
 
